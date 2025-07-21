ba905a59da670c918a6df3c59e8f1cb12982ebc7
Rohan-756 <rohansuresh2005@gmail.com>
2025-07-21 17:54:05 +0530

    Initial commit

---
+++ client/app/page.tsx
@@ -0,0 +1,109 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Menu } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      {/* top gradient */}
+
+      <div className="w-1/1 h-0 shadow-[0_0_200px_100px_rgba(0,0,0,0.5)] shadow-[#c800ff]"></div>
+
+      {/* nav bar */}
+
+      <nav className="flex justify-between p-3">
+        {/* logo */}
+        <div className="">
+          <img className="w-1/6 max-w-100 min-w-15 h-auto mr-2" src="website-logo.png" />
+        </div>
+        {/* buttons */}
+        <div className="flex gap-4 justify-center items-center max-sm:gap-2">
+          <Button className="text-white font-semibold rounded-full 
+        bg-[#1ed760] hover:bg-[#1db954] cursor-pointer active:bg-[#1db954] flex 
+        justify-center items-center h-min max-sm:text-xs max-sm:px-[6px]">
+            Login with Spotify
+          </Button>
+          <Menu className="text-white" />
+        </div>
+      </nav>
+
+      {/* header line */}
+
+      <div className="mx-auto w-fit text-white my-[80px]">
+        <h1 className="text-6xl md:text-4xl sm:text-3xl max-sm:text-2xl flex flex-col justify-center items-start">
+          <span>Get to know your</span>
+          <div className="flex gap-2 items-center">
+            <img className="w-auto h-15 md:h-12 sm:h-10 max-sm:h-8" src="spotify-logo.png" />
+            <span>stats — For Free</span>
+          </div>
+        </h1>
+      </div>
+
+      {/* grid box */}
+
+      <div className="grid grid-cols-2 gap-6 gap-y-8 mx-auto w-3/4 max-w-[800px] my-[20px] max-sm:grid-cols-1">
+        <div className="text-white font-semibold text-justify flex flex-col justify-center max-sm:order-2">
+          <span className="text-2xl font-semibold mb-[2px]">Your music taste is one of a kind.</span>
+          Dive into your stats, explore your most played tracks, discover hidden gems,
+          and see how your listening habits evolve — all in one place, beautifully visualized
+        </div>
+        <div className="max-sm:order-1"><img className=" rounded-xl shadow-2xl shadow-slate-800" src="man_listening_songs.jpg"></img></div>
+        <div className="max-sm:order-3"><img className=" rounded-xl shadow-2xl shadow-slate-800" src="woman_listening_songs.jpg"></img></div>
+        <div className="text-white font-semibold text-justify flex flex-col justify-center max-sm:order-4">
+          <section>
+            <span className="text-2xl font-semibold mb-[2px]">Connect your Spotify to begin.</span><br />
+            We'll pull in your listening data securely and give you a fresh,
+            fun look at your music story — no playlists touched, just insights delivered.
+          </section>
+          <Button className="text-white text-md p-[10px] font-semibold 
+          rounded-full bg-[#1ed760] hover:bg-[#1db954] cursor-pointer active:bg-[#1db954] col-2 mx-auto
+          flex justify-center items-center w-2/3 h-min min-w-fit mt-8">
+            Discover yourself!
+            <ArrowRight />
+          </Button>
+        </div>
+      </div>
+
+      {/* footer */}
+
+      <footer className="bg-gray-800 w-1/1 h-auto text-gray-400 p-3 mt-20">
+        {/* logo */}
+        <img className="w-20 opacity-60" src="website-logo.png" />
+        {/* grid box */}
+        <div className="grid grid-cols-[5fr_1fr] max-sm:grid-cols-1 p-2">
+          {/* left half */}
+          <div>
+            <div className="text-sm">
+              © 2025 Statify · Built with ❤️ for music lovers · Not affiliated with Spotify
+            </div>
+            <div className="col-1 text-sm m-5 ml-0">
+              This app uses Spotify's public API to access user data.
+              We do not store or share your personal information.
+              Spotify is a registered trademark of Spotify AB.
+            </div>
+          </div>
+
+          {/* right half */}
+          <section className="flex justify-center max-sm:justify-start">
+            <div className="flex flex-col items-start justify-start text-sm">
+              <li className="list-none hover:underline cursor-pointer active:underline">
+                About
+              </li>
+              <li className="list-none hover:underline cursor-pointer active:underline">
+                <a href="https://github.com/Rohan-756/Statify" target="_blank"
+                  rel="noopener noreferrer">
+                  GitHub
+                </a>
+              </li>
+            </div>
+          </section>
+
+        </div>
+
+
+      </footer>
+
+
+    </>
+  );
+}
