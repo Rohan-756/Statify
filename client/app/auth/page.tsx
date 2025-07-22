089d5863539e92eddbd8d5e0919a582229073bd8
Rohan-756 <rohansuresh2005@gmail.com>
2025-07-23 00:26:38 +0530

    Added authentication page while redirecting

--- client/app/auth/page.tsx
+++ client/app/auth/page.tsx
@@ -4,34 +4,48 @@ import React, { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
 const Page = () => {
-  const searchParams = useSearchParams();
-  const code = searchParams.get("code");
+    const searchParams = useSearchParams();
+    const code = searchParams.get("code");
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch(`/api/callback?code=${code}`, {
-          method: "GET",
-          credentials: "include", // ✅ ensures cookies are sent and received
-        });
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await fetch(`/api/callback?code=${code}`, {
+                    method: "GET",
+                    credentials: "include", // ✅ ensures cookies are sent and received
+                });
 
-        if (!response.ok) {
-          throw new Error(`Error ${response.status}: ${response.statusText}`);
-        }
+                if (!response.ok) {
+                    throw new Error(`Error ${response.status}: ${response.statusText}`);
+                }
 
-        // const data = await response.json();
-        // console.log("User data:", data); // ✅ Logs data
-      } catch (err) {
-        console.error("Fetch error:", err);
-      }
-    };
+                // const data = await response.json();
+                console.log("Authentication complete");
+            } catch (err) {
+                console.error("Authentication failed", err);
+            }
+        };
 
-    fetchUserData();
-  }, []);
+        fetchUserData();
+    }, []);
 
 
 
-  return <div className="text-white">Authenticating...</div>;
+    return (
+        <>
+
+            {/* shadow effect */}
+
+            <div className="w-full h-0 shadow-[0_0_200px_100px_rgba(0,0,0,0.5)]
+          shadow-[#c800ff] fixed -z-2"></div>
+            {/* authenticating message */}
+
+            <div className="text-white flex flex-col justify-center items-center w-screen h-screen">
+                <div className="text-2xl font-semibold">Authenticating.</div>
+                <div className="text-xl font-semibold">Please Wait...</div>
+            </div>
+
+        </>)
 };
 
 export default Page;
