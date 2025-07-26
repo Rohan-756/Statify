51d8d499d5e42a86c8ad6912254164951ba38ff0
Rohan-756 <rohansuresh2005@gmail.com>
2025-07-26 23:56:57 +0530

    Cleaned up stats page code with components

---
+++ client/app/stats/TopTracks.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+import "./stats.css"
+
+type Artist = {
+    name: string;
+}
+
+type AlbumImage = {
+    url: string;
+}
+
+type Album = {
+    images: AlbumImage[];
+}
+
+type ExternalUrls = {
+    spotify: string;
+}
+
+type Track = {
+    name: string;
+    artists: Artist[];
+    album: Album;
+    external_urls: ExternalUrls;
+}
+
+const TopTracks = () => {
+    const [tracks, setTracks] = useState<Track[]>([]);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch('/api/top-tracks', {
+            method: 'GET',
+            credentials: 'include', // This tells the browser to send cookies
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log('Received from /testing:', data);
+                // const names= Object.values(data.items).map(item => item.name);
+                setTracks(data.items || []);
+                console.log(tracks);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error('Error fetching /testing:', err);
+                setLoading(false);
+            });
+    }, []);
+    return (
+        <>
+            {/* top tracks heading */}
+            <h1 className="m-3 mt-10 lg:text-3xl md:text-3xl sm:text-3xl max-sm:text-2xl text-slate-800 dark:text-white">Your Top Tracks.</h1>
+            {/* list of items */}
+            {
+                (!loading) ?
+                    <>
+                        <div className="mt-3">
+                            {
+                                tracks.map((track, index) => (
+                                    <li key={index} className="flex m-2 whitespace-nowrap items-center justify-start gap-4 bg-[rgba(215,215,215,0.4)] dark:bg-[rgba(68,68,68,0.5)] rounded-md p-2 backdrop-blur-md z-1">
+                                        <div className="text-center min-w-5 ">{++index}</div>
+                                        <img className="max-sm:w-[50px] rounded-sm" src={track.album.images[2].url} />
+                                        <div className="flex flex-col overflow-hidden text-ellipsis">
+                                            <a href={track.external_urls.spotify} target="_blank" className="text-slate-900 dark:text-white">
+                                                <span className="overflow-hidden text-ellipsis hover:underline active:underline cursor-pointer">
+                                                    {track.name}
+                                                </span>
+                                            </a>
+                                            <span className="overflow-hidden text-ellipsis text-slate-800 dark:text-white">
+                                                {track.artists.map(artist => artist.name).join(", ")}
+                                            </span></div>
+                                    </li>
+                                ))
+                            }
+                        </div>
+                        <div className="text-2xl font-semibold text-slate-800 dark:text-white
+               w-full text-center my-8">That's It For Now!</div>
+                    </>
+                    : <>
+                        <div className="flex flex-col items-center  mt-8 w-full h-screen">
+                            <div className="spinning-disk"></div>
+                            <div className="text-xl">Loading...</div>
+                        </div>
+                    </>
+            }
+        </>
+    )
+}
+
+export default TopTracks
--- client/app/stats/page.jsx
+++ client/app/stats/page.jsx
@@ -3,31 +3,11 @@
 import Footer from '@/components/Footer';
 import NavBar from '@/components/NavBar';
 import TopShadow from '@/components/TopShadow';
+import TopTracks from './TopTracks';
 import {React, useState, useEffect} from "react";
 
 export default function StatsPage() {
-  const [tracks, setTracks] = useState([]);
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    fetch('/api/top-tracks', {
-      method: 'GET',
-      credentials: 'include', // This tells the browser to send cookies
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('Received from /testing:', data);
-        // const names= Object.values(data.items).map(item => item.name);
-        setTracks(data.items || []);
-        console.log(tracks);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error('Error fetching /testing:', err);
-        setLoading(false);
-      });
-  }, []);
-
+  
   return (
     <>
 
@@ -48,43 +28,7 @@ export default function StatsPage() {
           <span className="lg:text-5xl md:text-5xl sm:text-4xl max-sm:text-3xl font-semibold text-slate-200 dark:text-[rgb(61,34,71)]">All in one place.</span>
         </div>
         {/* output data */}
-        {/* top tracks heading */}
-        <h1 className="m-3 mt-10 lg:text-3xl md:text-3xl sm:text-3xl max-sm:text-2xl text-slate-800 dark:text-white">Your Top Tracks.</h1>
-        {/* list of items */}
-        {
-          (!loading) ?
-            <>
-              <div className="mt-3">
-                {
-                  tracks.map((track, index) => (
-                    <li key={index} className="flex m-2 whitespace-nowrap items-center justify-start gap-4 bg-[rgba(215,215,215,0.4)] dark:bg-[rgba(68,68,68,0.5)] rounded-md p-2 backdrop-blur-md z-1">
-                      <div className="text-center min-w-5 ">{++index}</div>
-                      <img className="max-sm:w-[50px] rounded-sm" src={track.album.images[2].url} />
-                      <div className="flex flex-col overflow-hidden text-ellipsis">
-                        <a href={track.external_urls.spotify} target="_blank" className="text-slate-900 dark:text-white">
-                          <span className="overflow-hidden text-ellipsis hover:underline active:underline cursor-pointer">
-                            {track.name}
-                          </span>
-                        </a>
-                        <span className="overflow-hidden text-ellipsis text-slate-800 dark:text-white">
-                          {track.artists.map(artist => artist.name).join(", ")}
-                        </span></div>
-                    </li>
-                  ))
-                }
-              </div>
-              <div className="text-2xl font-semibold text-slate-800 dark:text-white
-               w-full text-center my-8">That's It For Now!</div>
-            </>
-            : <>
-              <div className="flex flex-col items-center mt-8">
-                <div className="text-lg">Loading...</div>
-                <div className="text-lg">Please wait.</div>
-              </div>
-            </>
-        }
-
-
+        <TopTracks/>
       </main>
 
       <Footer />
