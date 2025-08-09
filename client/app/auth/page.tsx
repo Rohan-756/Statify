b9c09920c097eb2e5bb1d843aa32878d7318c99c
Rohan-756 <rohansuresh2005@gmail.com>
2025-08-09 21:51:08 +0530

    test: Added froce dynamic to auth page

--- client/app/auth/page.tsx
+++ client/app/auth/page.tsx
@@ -79,6 +79,9 @@
 
 "use client";
 
+export const dynamic = 'force-dynamic';
+
+
 import React, { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import "./auth.css";
